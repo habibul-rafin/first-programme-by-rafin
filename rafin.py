109995d1132dac89fa2f9c3f5ba73e4c6f33135d
habibul-rafin <77139942+habibul-rafin@users.noreply.github.com>
2021-01-08 12:11:30 +0600

    Create rafin.py

---
+++ rafin.py
@@ -0,0 +1,9 @@
+print("This is a simple Age Calculetor By Rafin")
+
+A = input("What Is Your Birth Year? ")
+
+age = 2020 - int(A)
+
+print(age)
+
+print("Thanks For Using Rafin's First Python Programme")
